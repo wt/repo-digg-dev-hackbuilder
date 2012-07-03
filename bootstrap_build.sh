7b741ec91f30f6a642378c9b8d270fc7cbecc779
Warren Turkal <wt@ooyala.com>
2012-07-03 17:35:46 +0000

    Output an example alias at the end of the bootstrap build script.

--- bootstrap_build.sh
+++ bootstrap_build.sh
@@ -2,4 +2,7 @@
 cd bootstrap_build/; ./build.sh clean; ./build.sh build; cd ..
 ../../../bootstrap-build/digg/dev/hackbuilder/-hack/python_virtualenv/bin/hack clean
 ../../../bootstrap-build/digg/dev/hackbuilder/-hack/python_virtualenv/bin/hack build :hack
-#../../../bootstrap-build/digg/dev/hackbuilder/-hack/python_virtualenv/bin/hack build :digg-hackbuilder
+echo '************************************************************************************************'
+echo "Here's an alias to make using the bootstrap version of hack easier:"
+echo "alias hack=${PWD}/../../../bootstrap-build/digg/dev/hackbuilder/-hack/python_virtualenv/bin/hack"
+echo '************************************************************************************************'
